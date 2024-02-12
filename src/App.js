9f6ca1c16b204cc996705b86dc439564aa148fe3
jocelynlugo <jocelynblugo@gmail.com>
2024-02-12 13:33:54 -0500

    added the Github repo url

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App </h1>
+      <footer>
+        This project was coded by{" "}
+        <a
+          href="https://jocelynlugo.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Jocelyn Lugo
+        </a>{" "}
+        and is open-sourced on{" "}
+        <a
+          href="https://github.com/jocelynlugo/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Github
+        </a>
+      </footer>
+    </div>
+  );
 }
